74af060ffb31f8390591f1c29c87352724548ee0
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-10 15:11:18 +0100

    fixed audio files aswell

--- gameView/gameController4.js
+++ gameView/gameController4.js
@@ -1,8 +1,8 @@
 //Lyd for hopping
 
-const jumpSound = new Audio('./gameView/gameSounds/jumpsound.wav');
-const coinSound = new Audio('./gameView/gameSounds/coinSound.mp3');
-const deathSound = new Audio('./gameView/gameSounds/deathSound.mp3');
+const jumpSound = new Audio('./gameSounds/jumpsound.wav');
+const coinSound = new Audio('./gameSounds/coinSound.mp3');
+const deathSound = new Audio('./gameSounds/deathSound.mp3');
 
 // {/* <audio id="jumpSound" src="/gameSounds/jumpsound.wav" preload="auto"></audio> */}
 
